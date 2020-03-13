b411058c175b8189a97067bb7baae1b7c1be1ba1
Brett Sevy <justbrettt@gmail.com>
2020-03-13 11:05:01 -0500

    commented out use public

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 //Routes
 require("./routes/api.js")(app);
